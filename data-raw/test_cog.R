449547399b8e734a69f49828d4f657da6ec4ca51
Michael Sumner <mdsumner@gmail.com>
2023-01-22 12:29:08 +1100

    better cog

--- data-raw/test_cog.R
+++ data-raw/test_cog.R
@@ -1,7 +1,10 @@
 ## code to prepare `DATASET` dataset goes here
 
 ## run in gdal 3.7
-system(sprintf("gdalwarp %s inst/extdata/world_cog_test.tif -overwrite -nomd -co COMPRESS=DEFLATE -co COMPRESS_OVERVIEW=DEFLATE -ot Int16 -te -180 -90 180 90 -ts 720 360 -t_srs 'OGC:CRS84' -of COG -co OVERVIEWS=IGNORE_EXISTING", 
-               #whatarelief::gebco21()), 
+
+system(sprintf("gdal_translate %s world_cog_test1.tif   -ot Int16 -projwin -180 90 180 -90 -outsize 720 360 -of COG -co OVERVIEWS=NONE -co TILED=NO", 
+             
                "/vsicurl/https://public.services.aad.gov.au/datasets/science/GEBCO_2021_GEOTIFF/GEBCO_2021.tif"))
-#system("gdaladdo inst/extdata/world_cog_test.tif 2 4 8 --config COMPRESS_OVERVIEW DEFLATE")
+              
+system("gdalwarp world_cog_test1.tif world_cog_test.tif -overwrite -nomd -co PREDICTOR=2 -co COMPRESS=LZW")
+system("gdaladdo world_cog_test.tif 2 4 8 --config COMPRESS_OVERVIEW LZW")
